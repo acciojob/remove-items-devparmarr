76e3cd4196aafafd4c6b1790b4c741a3cc2efb41
Dev Parmar <89907756+devparmarr@users.noreply.github.com>
2023-08-07 00:01:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const cs= document.getElementById("colorSelect");
+const btn= document.querySelector("input");
+const selectedOption=cs.options[cs.selectedIndex];
+
+
+function removeBtn() {
+	if(selectedOption){
+		cs.removeChild(selectedOption);
+	}
+}
+
+btn.addEventListener("click",removeBtn);
