e3f4b3b7b0944088ddb568914a36ade9012132dc
Dev Parmar <89907756+devparmarr@users.noreply.github.com>
2023-08-07 00:10:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
-//your JS code here. If required.
-const cs= document.getElementById("colorSelect");
-const btn= document.querySelector("input");
-const selectedOption=cs.options[cs.selectedIndex];
-let options=documnet.getElementByTagName("option");
 
-function removeBtn() {
-	if(selectedOption){
-		let optionValue = colorSelect.value;
-		for(let option of options){
-			if(option.value==optionValue)
-				cs.removeChild(selectedOption);
-		}
-		
-	}
+let colorSelect = document.getElementById('colorSelect');
+
+function removeColor() {
+    let optionValue = colorSelect.value;
+    let options = document.getElementsByTagName('option');
+
+
+    for (let option of options) {
+        if (option.value === optionValue) {
+            option.remove();
+        }
+    }
 }
-
-btn.addEventListener("click",removeBtn);
